c0ba2c77e9d2a5e2e37d06c833fdf9fc1e68106f
skanga <skanga@gmail.com>
2025-07-30 17:31:40 -0700

    Release version 3.0.4

--- RELEASE.bat
+++ RELEASE.bat
@@ -1,5 +1,5 @@
 
-set RELEASE_VER=3.0.2
+set RELEASE_VER=3.0.4
 
 :: 1. Update version in pom.xml
 call mvn versions:set -DnewVersion=%RELEASE_VER%
@@ -9,6 +9,6 @@ call mvn versions:commit
 call mvn clean package
 
 :: 3. Commit and push to main
-git add pom.xml DIR.md INSTALL.md README.md RELEASE.bat src/main/java/com/skanga/mcp/CliUtils.java
+git add pom.xml DIR.md INSTALL.md README.md RELEASE.bat src/main/java/com/skanga/mcp/config/CliUtils.java
 git commit -m "Release version %RELEASE_VER%"
 git push origin main
